4526c8628ed98836e67b29774fc1ff28bf1f1430
ILoveTheWitcherX <Gandhimanzano@gmail.com>
2023-10-18 15:26:43 -0600

    factor

--- temp.py
+++ temp.py
@@ -1,12 +1,17 @@
 
 
+#add another function to do fah to celcius
 
-
-def celsius_to_farenheit(celsius):
+def f_to_c(celsius):
   """given a celsius value return the conversion"""
   fahrenheit= celsius * 9/5 + 32 
-  return fahrenheit
+  return ( f + 32)*5/9
 
 celsius = 25
 fahrenheit = celsius_to_farenheit(celsius)
 print(f"{celsius} Celsius to Fahrenheit is {fahrenheit}")
+
+
+celsius = f_to_c(fahrenheit)
+print(f"{fahrenheit} Fahrenheit to celcius is {celsius}")
+
