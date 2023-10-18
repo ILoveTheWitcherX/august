de3a082bf994368fc4b664cca4dd7605a5a228b4
ILoveTheWitcherX <Gandhimanzano@gmail.com>
2023-10-18 14:46:18 -0600

    refactor code to make use of a function

--- temp.py
+++ temp.py
@@ -1,3 +1,11 @@
+
+
+
+
+def celsius_to_farenheit(celsius):
+  fahrenheit= celsius * 9/5 + 32 
+  return fahrenheit
+
 celsius = 25
-fahrenheit = celsius * 9/5 + 32
+fahrenheit = celsius_to_farenheit(celsius)
 print(f"{celsius} Celsius to Fahrenheit is {fahrenheit}")
